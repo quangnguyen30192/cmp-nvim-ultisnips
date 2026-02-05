43ee7b9b112511f8442324773cc7680aa94cd20e
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2026-02-05 17:15:46 +0100

    Update deprecated usage of vim.validate
    Neovim warns:
    
        WARNING vim.validate is deprecated. Feature will be removed in Nvim 1.0
    
    Use the non-deprecated syntax.

--- lua/cmp_nvim_ultisnips/config.lua
+++ lua/cmp_nvim_ultisnips/config.lua
@@ -8,25 +8,24 @@ M.default_config = {
   filetype_source = "treesitter",
 }
 
+local function is_valid_show_snippets(arg)
+  return arg == "expandable" or arg == "all"
+end
+
+local function is_valid_filetype_source(arg)
+  return arg == "treesitter" or arg == "ultisnips_default"
+end
+
 function M.get_user_config(config)
   local user_config = vim.tbl_deep_extend("force", M.default_config, config)
-  vim.validate {
-    show_snippets = {
-      user_config.show_snippets,
-      function(arg)
-        return arg == "expandable" or arg == "all"
-      end,
-      "either 'expandable' or 'all'",
-    },
-    documentation = { user_config.documentation, "function" },
-    filetype_source = {
-      user_config.filetype_source,
-      function(arg)
-        return arg == "treesitter" or arg == "ultisnips_default"
-      end,
-      "either 'treesitter' or 'ultisnips_default'",
-    },
-  }
+  vim.validate("show_snippets", user_config.show_snippets, is_valid_show_snippets, "either 'expandable' or 'all'")
+  vim.validate("documentation", user_config.documentation, "function")
+  vim.validate(
+    "filetype_source",
+    user_config.filetype_source,
+    is_valid_filetype_source,
+    "either 'treesitter' or 'ultisnips_default'"
+  )
   return user_config
 end
 
