quangnguyen30192/cmp-nvim-ultisnips

commit f5c5cd6da094ef04a7d6e0bea73f71dfa5dde9bf

refactor: extract Python code to separate file

autoload/cmp_nvim_ultisnips.vim
@@ -1,73 +1,3 @@
-" TODO: move python code into separate files
-
-" Retrieves additional snippet information that is not directly accessible
-" using the UltiSnips API functions. Returns a list of tables (one table
-" per snippet) with the keys "trigger", "description", "options" and "value".
-"
-" If 'expandable_only' is "True", only expandable snippets are returned, otherwise all
-" snippets except regex and custom context snippets for the current filetype are returned.
-function! cmp_nvim_ultisnips#get_current_snippets(expandable_only)
-let g:_cmpu_current_snippets = []
-python3 << EOF
-import vim
-from UltiSnips import UltiSnips_Manager, vim_helper
-
-before = vim_helper.buf.line_till_cursor
-visual_content = UltiSnips_Manager._visual_content
-expandable_only = vim.eval("a:expandable_only") == "True"
-if expandable_only:
-    snippets = UltiSnips_Manager._snips(before, True)
-else:
-    snippets = UltiSnips_Manager._snips("", True)
-
-for snippet in snippets:
-    is_context_snippet = snippet._context_code != None
-    is_regex_snippet = "r" in snippet._opts
-    # If show_snippets == "all", the snippets are cached so ignore "dynamic" snippets.
-    if not expandable_only and (is_context_snippet or is_regex_snippet):
-      continue
-    # For custom context snippets, always check if the context matches.
-    if is_context_snippet and not snippet._context_match(visual_content, before):
-        continue
-
-    vim.command(
-      "call add(g:_cmpu_current_snippets, {"
-      "'trigger': py3eval('snippet._trigger'),"
-      "'description': py3eval('snippet._description'),"
-      "'options': py3eval('snippet._opts'),"
-      "'value': py3eval('snippet._value'),"
-      "'matched': py3eval('snippet._matched'),"
-      "})"
-    )
-EOF
-return g:_cmpu_current_snippets
-endfunction
-
-function cmp_nvim_ultisnips#set_filetype(filetype)
-python3 << EOF
-import vim
-from UltiSnips import vim_helper
-
-filetype = vim.eval("a:filetype")
-class CustomVimBuffer(vim_helper.VimBuffer):
-  @property
-  def filetypes(self):
-    return [filetype]
-
-vim_helper._orig_buf = vim_helper.buf
-vim_helper.buf = CustomVimBuffer()  # TODO: avoid creating a new class instance every time
-EOF
-endfunction
-
-function! cmp_nvim_ultisnips#reset_filetype()
-python3 << EOF
-from UltiSnips import vim_helper
-
-# Restore to original VimBuffer instance
-vim_helper.buf = vim_helper._orig_buf
-EOF
-endfunction
-
 function! cmp_nvim_ultisnips#setup_treesitter_autocmds()
   augroup cmp_nvim_ultisnips
     autocmd!

lua/cmp_nvim_ultisnips/init.lua
@@ -12,6 +12,7 @@ local source
 function M.create_source()
   -- Source UltiSnips file in case it is not loaded yet (GH issue #49)
   vim.cmd("runtime! plugin/UltiSnips.vim")
+  vim.cmd("py3 import ultisnips_utils")
   -- This is necessary because we want to be able to define our own
   -- select mode mappings used to jump between snippet tabstops (GH issue #5)
   vim.g.UltiSnipsRemoveSelectModeMappings = 0

lua/cmp_nvim_ultisnips/snippets.lua
@@ -9,13 +9,12 @@ function M.load_snippets(expandable_only)
   if expandable_only then
     -- Do not cache snippets since the set of expandable
     -- snippets can change on every keystroke.
-    return vim.fn["cmp_nvim_ultisnips#get_current_snippets"](true)
+    return vim.fn.pyeval("ultisnips_utils.fetch_current_snippets(True)")
   end
   local ft = vim.bo.filetype
   local snippets = snippets_for_ft[ft]
   if not snippets then
-    snippets = vim.fn["cmp_nvim_ultisnips#get_current_snippets"](false)
-    snippets_for_ft[ft] = snippets
+    snippets_for_ft[ft] = vim.fn.pyeval("ultisnips_utils.fetch_current_snippets(False)")
   end
   return snippets
 end

lua/cmp_nvim_ultisnips/treesitter.lua
@@ -34,14 +34,14 @@ local cur_ft_at_cursor
 function M.set_filetype()
   local new_ft = get_ft_at_cursor()
   if new_ft ~= nil and new_ft ~= cur_ft_at_cursor and new_ft ~= vim.bo.filetype then
-    vim.fn["cmp_nvim_ultisnips#set_filetype"](new_ft)
+    vim.fn.pyeval(("ultisnips_utils.set_filetype(%s)"):format(new_ft))
     cur_ft_at_cursor = new_ft
   end
 end
 
 function M.reset_filetype()
   if cur_ft_at_cursor ~= nil then
-    vim.fn["cmp_nvim_ultisnips#reset_filetype"]()
+    vim.fn.pyeval("ultisnips_utils.reset_filetype()")
     cur_ft_at_cursor = nil
   end
 end

.gitignore
@@ -1,0 +1,1 @@
+__pycache__

python3/ultisnips_utils.py
@@ -1,0 +1,41 @@
+from UltiSnips import UltiSnips_Manager, vim_helper
+
+# Retrieves additional snippet information that is not directly accessible
+# using the UltiSnips API functions. Stores a list of dictionaries (one per
+# snippet) with the keys "trigger", "description", "options" and "value"
+# in the vim variable g:_cmpu_current_snippets.
+#
+# If 'expandable_only' is True, only expandable snippets are stored, otherwise
+# all snippets for the current filetype are added.
+
+
+def fetch_current_snippets(expandable_only):
+    if expandable_only:
+        snippets = UltiSnips_Manager._snips(before_line, True)
+    else:
+        snippets = UltiSnips_Manager._snips("", True)
+
+    current_snippet_infos = []
+    for snippet in snippets:
+        snippet_info = {
+            "trigger": snippet._trigger,
+            "description": snippet._description,
+            "options": snippet._opts,
+            "value": snippet._value,
+        }
+        current_snippet_infos.append(snippet_info)
+    return current_snippet_infos
+
+
+def set_filetype(ft):
+    class CustomVimBuffer(vim_helper.VimBuffer):
+        @property
+        def filetypes(self):
+            return [ft]
+
+    vim_helper._orig_buf = vim_helper.buf
+    vim_helper.buf = CustomVimBuffer()
+
+
+def reset_filetype():
+    vim_helper.buf = vim_helper._orig_buf

commit 41a7e2c219e6648c64853e41df4139822d8458b7

perf: defer import ultisnips_utils
Importing ultisnips_utils in cmp_nvim_ultisnips.init.create_source
causes nvim startup to take 150 ms more on my laptop. This is a
regression introduced in
<https://github.com/quangnguyen30192/cmp-nvim-ultisnips/pull/93>.

The fix is to defer the import until it's actually needed (the first
time the completion source is used).

Fixes: https://github.com/quangnguyen30192/cmp-nvim-ultisnips/issues/100
Fixes: f5c5cd6da094 ("refactor: extract Python code to separate file")

autoload/cmp_nvim_ultisnips.vim
@@ -29,3 +29,23 @@ function! cmp_nvim_ultisnips#initCustomUltiSnipMappings()
   smap <silent> <Plug>(cmpu-jump-backwards)
         \ <Esc>:call UltiSnips#JumpBackwards()<cr>
 endfunction
+
+" ------------------------------------------------------------------------
+" Wrappers for ultisnips_utils.py that defer the import to first use.
+" Saves 100+ ms of nvim startup time.
+" ------------------------------------------------------------------------
+
+function! cmp_nvim_ultisnips#fetch_current_snippets(expandable_only)
+  py3 import ultisnips_utils
+  return py3eval('ultisnips_utils.fetch_current_snippets(vim.eval("a:expandable_only"))')
+endfunction
+
+function! cmp_nvim_ultisnips#set_filetype(ft)
+  py3 import ultisnips_utils
+  py3 ultisnips_utils.set_filetype(vim.eval("a:ft"))
+endfunction
+
+function! cmp_nvim_ultisnips#reset_filetype()
+  py3 import ultisnips_utils
+  py3 ultisnips_utils.reset_filetype()
+endfunction

lua/cmp_nvim_ultisnips/init.lua
@@ -12,7 +12,6 @@ local source
 function M.create_source()
   -- Source UltiSnips file in case it is not loaded yet (GH issue #49)
   vim.cmd("runtime! plugin/UltiSnips.vim")
-  vim.cmd("py3 import ultisnips_utils")
   -- This is necessary because we want to be able to define our own
   -- select mode mappings used to jump between snippet tabstops (GH issue #5)
   vim.g.UltiSnipsRemoveSelectModeMappings = 0

lua/cmp_nvim_ultisnips/snippets.lua
@@ -9,12 +9,12 @@ function M.load_snippets(expandable_only)
   if expandable_only then
     -- Do not cache snippets since the set of expandable
     -- snippets can change on every keystroke.
-    return vim.fn.pyeval("ultisnips_utils.fetch_current_snippets(True)")
+    return vim.fn["cmp_nvim_ultisnips#fetch_current_snippets"](true)
   end
   local ft = vim.bo.filetype
   local snippets = snippets_for_ft[ft]
   if not snippets then
-    snippets = vim.fn.pyeval("ultisnips_utils.fetch_current_snippets(False)")
+    snippets = vim.fn["cmp_nvim_ultisnips#fetch_current_snippets"](false)
     snippets_for_ft[ft] = snippets
   end
   return snippets

lua/cmp_nvim_ultisnips/treesitter.lua
@@ -34,14 +34,14 @@ local cur_ft_at_cursor
 function M.set_filetype()
   local new_ft = get_ft_at_cursor()
   if new_ft ~= nil and new_ft ~= cur_ft_at_cursor and new_ft ~= vim.bo.filetype then
-    vim.fn.pyeval(('ultisnips_utils.set_filetype("%s")'):format(new_ft))
+    vim.fn["cmp_nvim_ultisnips#set_filetype"](new_ft)
     cur_ft_at_cursor = new_ft
   end
 end
 
 function M.reset_filetype()
   if cur_ft_at_cursor ~= nil then
-    vim.fn.pyeval("ultisnips_utils.reset_filetype()")
+    vim.fn["cmp_nvim_ultisnips#reset_filetype"]()
     cur_ft_at_cursor = nil
   end
 end